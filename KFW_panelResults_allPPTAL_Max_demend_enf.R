f57d7b3471a4d4b8ca560c3e493d89cda3906b2d
rtrichler <rbtrichler@ip-128-239-186-230.v4.wm.edu>
2017-05-03 10:15:53 -0400

    updating panel build, models, and output

--- KFW_panelResults_allPPTAL_Max_demend_enf.R
+++ KFW_panelResults_allPPTAL_Max_demend_enf.R
@@ -41,27 +41,33 @@ kfw_reshape1<-kfw_reshape[,-grep("(2011)",names(kfw_reshape))]
 kfw_reshape2<-kfw_reshape1[,-grep("(2012)",names(kfw_reshape1))]
 kfw_reshape3<-kfw_reshape2[,-grep("(2013)",names(kfw_reshape2))]
 kfw_reshape4<-kfw_reshape3[,-grep("(2014)",names(kfw_reshape3))]
+kfw_reshape5<-kfw_reshape4[,-grep("(2015)",names(kfw_reshape4))]
+kfw_reshape6<-kfw_reshape5[,-grep("(2020)",names(kfw_reshape5))]
 
-kfw_wide<-kfw_reshape4
+kfw_wide<-kfw_reshape6
 
 #Order variables chronologically to allow reshape to work properly
 kfw_wide<-kfw_wide[,order(names(kfw_wide))]
 
 #Identify variables where values will change yearly in panel dataset
-MeanT<-grep("MeanT_",names(kfw_wide))
-MeanP<-grep("MeanP_",names(kfw_wide))
-MinT<-grep("MinT_",names(kfw_wide))
-MaxT<-grep("MaxT_",names(kfw_wide))
-MinP<-grep("MinP_",names(kfw_wide))
-MaxP<-grep("MaxP_",names(kfw_wide))
-MaxL<-grep("MaxL_",names(kfw_wide))
-Pop<-grep("Pop_",names(kfw_wide))
+MeanT_<-grep("MeanT_",names(kfw_wide))
+MeanP_<-grep("MeanP_",names(kfw_wide))
+MinT_<-grep("MinT_",names(kfw_wide))
+MaxT_<-grep("MaxT_",names(kfw_wide))
+MinP_<-grep("MinP_",names(kfw_wide))
+MaxP_<-grep("MaxP_",names(kfw_wide))
+MaxL_<-grep("MaxL_",names(kfw_wide))
+MeanL_<-grep("MeanL_",names(kfw_wide))
+MedL_<-grep("MedL_",names(kfw_wide))
+Pop_<-grep("Pop_",names(kfw_wide))
 
 #Convert to long form panel dataset
-all_reshape <- c(MeanT,MeanP,MaxT,MaxP,MinP,MinT,MaxL,Pop)
-psm_Long <- reshape(kfw_wide@data, varying=all_reshape, direction="long",idvar="reu_",sep="_",timevar="Year")
+all_reshape <- c(MeanT_,MeanP_,MinT_,MaxT_,MinP_,MaxP_,MaxL_,MeanL_,MedL_,Pop_)
+psm_Long <- reshape(kfw_wide@data, varying=all_reshape, direction="long",idvar="id",sep="_",timevar="Year")
 
-#---
+#--------------------------
+# Construct Treatment Vars
+#--------------------------
 #Create years to demarcation
 psm_Long$yrtodem <- NA
 psm_Long$yrtodem=psm_Long$Year - psm_Long$demend_y
@@ -96,10 +102,57 @@ psmtest5$trtenf[is.na(psmtest5$demend_y)]<-0
 table(psmtest5$trtenf,psmtest5$trtdem)
 
 psm_Long <- psmtest5
-#-
+
 #Create post-2004 dummy
 psm_Long$Post2004 <- 0
 psm_Long$Post2004[psm_Long$Year >= 2004] <- 1
+
+#----------------------
+# Regression Models
+#----------------------
+
+
+pModelMax_A <- "MaxL ~ trtdem + trtenf + factor(reu_id)"
+pModelMax_B <- "MaxL ~ trtdem + trtenf + Pop + MeanT + MeanP + MaxT + MaxP + MinT + MinP  + factor(reu_id) "
+pModelMax_C <- "MaxL ~ trtdem + trtenf + Pop + MeanT + MeanP + MaxT + MaxP + MinT + MinP  + Year + factor(reu_id)"
+pModelMax_C2 <- "MaxL ~ trtdem + trtenf + Pop + MeanT + MeanP+ MaxT + MaxP + MinT + MinP  + factor(Year) + factor(reu_id)"
+
+pModelMax_D <- "MaxL ~ trtdem + Pop+ MeanT + MeanP+ MaxT + MaxP + MinT + MinP + factor(Year) + factor(reu_id) + Post2004 + Post2004*trtdem + Post2004*trtdem*Road_dist + Post2004*Road_dist"
+pModelMax_E <- "MaxL ~ trtdem + trtenf+ Pop + MeanT + MeanP + MaxT + MaxP + MinT + MinP + factor(Year) + factor(reu_id) + Post2004 + Post2004*trtdem + Post2004*trtdem*Road_dist + Post2004*Road_dist"
+
+
+pModelMax_A_fit <- Stage2PSM(pModelMax_A ,psm_Long,type="cmreg", table_out=TRUE,opts=c("reu_id","Year"))
+pModelMax_B_fit <- Stage2PSM(pModelMax_B ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
+pModelMax_C_fit <- Stage2PSM(pModelMax_C ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
+pModelMax_C2_fit <- Stage2PSM(pModelMax_C2 ,psm_Long,type="cmreg", table_out=TRUE,opts=c("reu_id","Year"))
+
+pModelMax_D_fit <- Stage2PSM(pModelMax_D ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
+pModelMax_E_fit <- Stage2PSM(pModelMax_E ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
+
+
+## stargazer output with variable labels
+
+#Used for JEEM 2nd resubmission
+#imported html into excel and did some reformatting within
+stargazer(pModelMax_A_fit$cmreg,pModelMax_B_fit$cmreg,pModelMax_C_fit$cmreg,
+          pModelMax_C2_fit$cmreg,pModelMax_D_fit$cmreg,pModelMax_E_fit$cmreg,
+          type="html", align=TRUE,
+          omit=c("factor","Constant"),
+          covariate.labels=c("Treatment (Demarcation)","Treatment (Demarcation + Enforcement Support)","Population","Mean Temp",
+                             "Mean Precip","Max Temp","Max Precip","Min Temp","Min Precip","Year",
+                             "Treatment(Dem)*Post 2004","Post 2004*Road Dist","Treatment(Dem)*Road Dist",
+                             "Treatment(Dem)*Post 2004*Road Dist"),
+          omit.stat=c("f","ser"),
+          add.lines=list(c("Observations","4379","4379","4379","4379","4379","4379"),
+                         c("Community Fixed Effects?","Yes","Yes","Yes","Yes","Yes","Yes"),
+                         c("Year Fixed Effects?","No","No","No","Yes","Yes","Yes")),
+          title="Regression Results",
+          dep.var.labels=c("Max NDVI"))
+
+
+#-----------------
+#Scratch/Archived
+#-----------------
 
 #Create arc of deforestation dummy
 psm_Long$arc<-NA
@@ -111,95 +164,18 @@ DemYears <- summaryBy(TrtMnt_demend_y~reu_id, data=psm_Long,FUN=c(mean,sum))
 psm_Long2 <- psm_Long
 psm_Long3 <- merge(psm_Long2, DemYears, by="reu_id")
 psm_Long<-psm_Long3
-#---
-
-pModelMax_A <- "MaxL_ ~ TrtMnt_demend_y + TrtMnt_enforce_st + factor(reu_id)"
-pModelMax_B <- "MaxL_ ~ TrtMnt_demend_y + TrtMnt_enforce_st + Pop_ + MeanT_ + MeanP_ + MaxT_ + MaxP_ + MinT_ + MinP_  + factor(reu_id) "
-pModelMax_C <- "MaxL_ ~ TrtMnt_demend_y + TrtMnt_enforce_st + Pop_ + MeanT_ + MeanP_ + MaxT_ + MaxP_ + MinT_ + MinP_  + Year + factor(reu_id)"
-pModelMax_C1 <- "MaxL_ ~ TrtMnt_demend_y + Pop_ + MeanT_ + MeanP_+ MaxT_ + MaxP_ + MinT_ + MinP_  + factor(Year) + factor(reu_id)"
-pModelMax_C2 <- "MaxL_ ~ TrtMnt_demend_y + TrtMnt_enforce_st + Pop_ + MeanT_ + MeanP_+ MaxT_ + MaxP_ + MinT_ + MinP_  + factor(Year) + factor(reu_id)"
-
-pModelMax_D <- "MaxL_ ~ TrtMnt_demend_y + Pop_+ MeanT_ + MeanP_ + MaxT_ + MaxP_ + MinT_ + MinP_ + factor(Year) + factor(reu_id) + Post2004 + Post2004*TrtMnt_demend_y + Post2004*TrtMnt_demend_y*Road_dist + Post2004*Road_dist"
-pModelMax_E <- "MaxL_ ~ TrtMnt_demend_y + Pop_+TrtMnt_enforce_st + MeanT_ + MeanP_ + MaxT_ + MaxP_ + MinT_ + MinP_ + factor(Year) + factor(reu_id) + Post2004 + Post2004*TrtMnt_demend_y + Post2004*TrtMnt_demend_y*Road_dist + Post2004*Road_dist"
 
 
-pModelMax_A_fit <- Stage2PSM(pModelMax_A ,psm_Long,type="cmreg", table_out=TRUE,opts=c("reu_id","Year"))
-pModelMax_B_fit <- Stage2PSM(pModelMax_B ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
-pModelMax_C_fit <- Stage2PSM(pModelMax_C ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
-pModelMax_C1_fit <- Stage2PSM(pModelMax_C1 ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
-pModelMax_C2_fit <- Stage2PSM(pModelMax_C2 ,psm_Long,type="cmreg", table_out=TRUE,opts=c("reu_id","Year"))
-
-pModelMax_D_fit <- Stage2PSM(pModelMax_D ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
-pModelMax_E_fit <- Stage2PSM(pModelMax_E ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
-
-#Interaction with number of years post demarcation (to 2010 end year)
-pModelMax_F <- "MaxL_ ~ TrtMnt_demend_y + Pop_ + MeanT_ + MeanP_+ MaxT_ + MaxP_ + MinT_ + MinP_  + TrtMnt_demend_y*factor(TrtMnt_demend_y.sum) + factor(Year) + factor(reu_id)"
-pModelMax_G <- "MaxL_ ~ TrtMnt_demend_y + TrtMnt_enforce_st + Pop_ + MeanT_ + MeanP_+ MaxT_ + MaxP_ + MinT_ + MinP_  + TrtMnt_demend_y*factor(TrtMnt_demend_y.sum) + factor(Year) + factor(reu_id)"
-
-pModelMax_F_fit <- Stage2PSM(pModelMax_F ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
-pModelMax_G_fit <- Stage2PSM(pModelMax_G ,psm_Long,type="cmreg", table_out=TRUE, opts=c("reu_id","Year"))
+pModelMax_A <- lm(MaxL_ ~ trtdem + trtenf , data=psm_Long)
+clusterA<-cluster.vcov(pModelMax_A,cbind(psm_Long$Year,psm_Long$id), force_posdef=TRUE)
+pModelMax_A_fit <- coeftest(pModelMax_A,clusterA)
 
 
-##texreg to output regression results visualizations
-texreg::plotreg(pModelMax_B_fit$cmreg, custom.model.names=c("Panel Results, Max NDVI"), 
-                omit.coef="(Pop_)|(Min)|(Mean)|(Max)|(Year)|(match)|(Intercept)|(factor)", 
-                custom.note="standard deviation")
-
-texreg::plotreg(pModelMax_C_fit$cmreg,custom.model.names=c("Panel Results, Max NDVI"), 
-                omit.coef="(Pop_)|(Min)|(Mean)|(Max)|(Year)|(Intercept)|(factor)", 
-                custom.note="standard deviation")
-
-## stargazer output with variable labels
-stargazer(pModelMax_A_fit$cmreg,pModelMax_B_fit$cmreg,pModelMax_C_fit$cmreg,pModelMax_D_fit$cmreg,pModelMax_E_fit$cmreg,type="html",align=TRUE,keep=c("TrtMnt_demend_y","TrtMnt_enforce_st","MeanT_","MeanP_","Pop_","MaxT_","MaxP_","MinT_","MinP_","Year","Post2004","TrtMnt_demend_y:Post2004","Post2004:Road_dist","TrtMnt_demend_y:Road_dist","TrtMnt_demend_y:Post2004:Road_dist"),
-          covariate.labels=c("Treatment (Demarcation)","Treatment (Enforcement Support)","Mean Temperature","Mean Precipitation","Population","Max Temperature","Max Precipitation","Min Temperature","Min Precipitation","Year","Post2004","Post2004*TreatmentDemarcation","Post2004*Road Distance","TreatmentDemarcation*Road Distance","Post2004*TreatmentDemarcation*Road Distance"),
-          omit.stat=c("f","ser"),
-          title="Regression Results",
-          dep.var.labels=c("Max NDVI")
-)
-
-
-stargazer(pModelMax_A_fit$cmreg,pModelMax_B_fit$cmreg,pModelMax_C_fit$cmreg,pModelMax_C1_fit$cmreg,pModelMax_D_fit$cmreg,pModelMax_E_fit$cmreg,
-          type="html",align=TRUE,
-          keep=c("TrtMnt_demend_y","TrtMnt_enforce_st","MeanT_","MeanP_","Pop_","MaxT_","MaxP_","MinT_","MinP_",
-                 "Year","Post2004","TrtMnt_demend_y:Post2004","Post2004:Road_dist","TrtMnt_demend_y:Road_dist",
-                 "TrtMnt_demend_y:Post2004:Road_dist"),
-          #           covariate.labels=c("Treatment (Demarcation)","Treatment (Enforcement Support)","Mean Temp",
-          #                              "Mean Precip","Population","Max Temperature","Max Precipitation","Min Temperature","Min Precipitation","Year","Post2004","Post2004*TreatmentDemarcation","Post2004*Road Distance","TreatmentDemarcation*Road Distance","Post2004*TreatmentDemarcation*Road Distance"),
-          omit.stat=c("f","ser"),
-          title="Regression Results",
-          dep.var.labels=c("Max NDVI")
-)
-
-stargazer(pModelMax_C1_fit$cmreg,pModelMax_F_fit$cmreg,pModelMax_G_fit$cmreg,
-          type="html", align=TRUE,
-          keep=c("TrtMnt","Pop","Mean","Max","Min","Year","factor"),
-          title="Regression Results",
-          dep.var.labels=c("Max NDVI"))
-
-#Used for JEEM submission
-stargazer(pModelMax_A_fit$cmreg,pModelMax_B_fit$cmreg,pModelMax_C_fit$cmreg,
-          pModelMax_C2_fit$cmreg,pModelMax_D_fit$cmreg,pModelMax_E_fit$cmreg,
-          type="html", align=TRUE,
-          keep=c("TrtMnt","Pop","Mean","Max","Min","Year","Post2004","Road_dist"),
-          covariate.labels=c("Treatment (Demarcation)","Treatment (Demarcation + Enforcement Support)","Population","Mean Temp",
-                             "Mean Precip","Max Temp","Max Precip","Min Temp","Min Precip","Year"),
-          omit.stat=c("f","ser"),
-          add.lines=list(c("Observations","1914","1914","1914","1914","1914","1914"),
-                         c("Community Fixed Effects?","Yes","Yes","Yes","Yes","Yes","Yes"),
-                         c("Year Fixed Effects?","No","No","No","Yes","Yes","Yes")),
-          title="Regression Results",
-          dep.var.labels=c("Max NDVI"))
-
-stargazer(pModelMax_A_fit$cmreg,pModelMax_B_fit$cmreg,pModelMax_C_fit$cmreg,
-          pModelMax_C1_fit$cmreg,pModelMax_C2_fit$cmreg,
-          type="html", align=TRUE,
-          keep=c("TrtMnt","Pop","Mean","Max","Min","Year"),
-          covariate.labels=c("Treatment (Demarcation)","Treatment (Demarcation + Enforcement Support)","Population","Mean Temp",
-                             "Mean Precip","Max Temp","Max Precip","Min Temp","Min Precip","Year"),
-          omit.stat=c("f","ser"),
-          add.lines=list(c("Observations","2146","2146","2146","2146","2146"),
-                         c("Community Fixed Effects?","Yes","Yes","Yes","Yes","Yes","Yes"),
-                         c("Year Fixed Effects?","No","No","No","Yes","Yes")),
-          title="Regression Results",
-          dep.var.labels=c("Max NDVI"))
+Model4<- lm(lfreq ~ trt_dem + ifreq+
+              Pop + 
+              ntl +
+              factor(year) + factor(id),
+            data=panel_data)
+cluster4 <- cluster.vcov(Model4, cbind(panel_data$year, panel_data$id), force_posdef=TRUE)
+pModelMax_A_fit <- coeftest(Model4, cluster4)
 
