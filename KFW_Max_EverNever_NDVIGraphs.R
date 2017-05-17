1e13cb6bf51db8b623f3ee5052d90abc17431650
rtrichler <rbtrichler@ip-128-239-134-94.v4.wm.edu>
2017-05-17 11:10:31 -0400

    remove extraneous code
    for matching data because now it should show all 151 communities

--- KFW_Max_EverNever_NDVIGraphs.R
+++ KFW_Max_EverNever_NDVIGraphs.R
@@ -19,60 +19,11 @@ loadLibs()
 shpfile = "processed_data/kfw_analysis_inputs.shp"
 dta_Shp = readShapePoly(shpfile)
 
-
-#-------------------------------------------------
-#-------------------------------------------------
-#Pre-processing to create cross-sectional variable summaries
-#-------------------------------------------------
-#-------------------------------------------------
-#Calculate NDVI Trends
-dta_Shp$pre_trend_NDVI_mean <- timeRangeTrend(dta_Shp,"MeanL_[0-9][0-9][0-9][0-9]",1982,1995,"id")
-dta_Shp$pre_trend_NDVI_max <- timeRangeTrend(dta_Shp,"MaxL_[0-9][0-9][0-9][0-9]",1982,1995,"id")
-dta_Shp$NDVIslope_95_10 <- timeRangeTrend(dta_Shp,"MaxL_[0-9][0-9][0-9][0-9]",1995,2010,"id")
-dta_Shp@data["NDVILevelChange_95_10"] <- dta_Shp$MaxL_2010 - dta_Shp$MaxL_1995
-
-#NDVI Trends for 1995-2001
-dta_Shp$post_trend_NDVI_95_01 <- timeRangeTrend(dta_Shp,"MaxL_[0-9][0-9][0-9][0-9]",1995,2001,"id")
-dta_Shp@data["NDVILevelChange_95_01"] <- dta_Shp$MaxL_2001 - dta_Shp$MaxL_1995
-#dta_Shp@data["NDVIslopeChange_95_01"] <- dta_Shp@data["post_trend_NDVI_95_01"] - dta_Shp@data["pre_trend_NDVI_max"]
-
-#NDVI Trends for 2001-2010
-dta_Shp$post_trend_NDVI_01_10 <- timeRangeTrend(dta_Shp,"MaxL_[0-9][0-9][0-9][0-9]",2001,2010,"id")
-dta_Shp@data["NDVILevelChange_01_10"] <- dta_Shp$MaxL_2010 - dta_Shp$MaxL_2001
-#dta_Shp@data["NDVIslopeChange_01_10"] <- dta_Shp@data["post_trend_NDVI_01_10"] - dta_Shp@data["pre_trend_NDVI_max"]
-
-#Calculate Temp and Precip Pre and Post Trends
-dta_Shp$pre_trend_temp_mean <- timeRangeTrend(dta_Shp,"MeanT_[0-9][0-9][0-9][0-9]",1982,1995,"id")
-dta_Shp$pre_trend_temp_max <- timeRangeTrend(dta_Shp,"MaxT_[0-9][0-9][0-9][0-9]",1982,1995,"id")
-dta_Shp$pre_trend_temp_min <- timeRangeTrend(dta_Shp,"MinT_[0-9][0-9][0-9][0-9]",1982,1995,"id")
-
-dta_Shp$post_trend_temp_mean <- timeRangeTrend(dta_Shp,"MeanT_[0-9][0-9][0-9][0-9]",1995,2010,"id")
-dta_Shp$post_trend_temp_max <- timeRangeTrend(dta_Shp,"MaxT_[0-9][0-9][0-9][0-9]",1995,2010,"id")
-dta_Shp$post_trend_temp_min <- timeRangeTrend(dta_Shp,"MinT_[0-9][0-9][0-9][0-9]",1995,2010,"id")
-dta_Shp$post_trend_temp_95_01 <- timeRangeTrend(dta_Shp,"MeanT_[0-9][0-9][0-9][0-9]",1995,2001,"id")
-dta_Shp$post_trend_temp_01_10 <- timeRangeTrend(dta_Shp,"MeanT_[0-9][0-9][0-9][0-9]",2001,2010,"id")
-
-dta_Shp$pre_trend_precip_mean <- timeRangeTrend(dta_Shp,"MeanP_[0-9][0-9][0-9][0-9]",1982,1995,"id")
-dta_Shp$pre_trend_precip_max <- timeRangeTrend(dta_Shp,"MaxP_[0-9][0-9][0-9][0-9]",1982,1995,"id")
-dta_Shp$pre_trend_precip_min <- timeRangeTrend(dta_Shp,"MinP_[0-9][0-9][0-9][0-9]",1982,1995,"id")
-
-dta_Shp$post_trend_precip_mean <- timeRangeTrend(dta_Shp,"MeanP_[0-9][0-9][0-9][0-9]",1995,2010,"id")
-dta_Shp$post_trend_precip_max <- timeRangeTrend(dta_Shp,"MaxP_[0-9][0-9][0-9][0-9]",1995,2010,"id")
-dta_Shp$post_trend_precip_min <- timeRangeTrend(dta_Shp,"MinP_[0-9][0-9][0-9][0-9]",1995,2010,"id")
-dta_Shp$post_trend_precip_95_01 <- timeRangeTrend(dta_Shp,"MeanP_[0-9][0-9][0-9][0-9]",1995,2001,"id")
-dta_Shp$post_trend_precip_01_10 <- timeRangeTrend(dta_Shp,"MeanP_[0-9][0-9][0-9][0-9]",2001,2010,"id")
-
-
-
-
-
-
 #-------------------------------------------------
 #-------------------------------------------------
 #Define the Treatment Variable and Population
 #-------------------------------------------------
 #-------------------------------------------------
-
 
 #Eliminate non-PPTAL indigenous lands
 dta_Shp@data$proj_check <- 0
@@ -95,51 +46,63 @@ View(demtable)
 
 #-------------------------------------------------
 #-------------------------------------------------
-#Define and run the first-stage of the PSM, calculating propensity scores
-#-------------------------------------------------
-#-------------------------------------------------
-psmModel <-  "TrtBin ~ terrai_are + Pop_1990 + MeanT_1995 + pre_trend_temp_mean + pre_trend_temp_min + 
-pre_trend_temp_max + MeanP_1995 + pre_trend_precip_min + 
-pre_trend_NDVI_mean + pre_trend_NDVI_max + Slope + Elevation + MaxL_1995 + Riv_Dist + Road_dist +
-pre_trend_precip_mean + pre_trend_precip_max"
-#MeanL_1995
-
-psmRes <- SAT::SpatialCausalPSM(dta_Shp,mtd="logit",psmModel,drop="support",visual=FALSE)
-
-#-------------------------------------------------
-#-------------------------------------------------
-#Based on the Propensity Score Matches, pair comprable treatment and control units.
-#-------------------------------------------------
-#-------------------------------------------------
-drop_set<- c(drop_unmatched=TRUE,drop_method="None",drop_thresh=0.5)
-psm_Pairs <- SAT(dta = psmRes$data, mtd = "fastNN",constraints=c(groups="UF"),psm_eq = psmModel, ids = "id", drop_opts = drop_set, visual="TRUE", TrtBinColName="TrtBin")
-#c(groups=c("UF"),distance=NULL)
-trttable <- table (psm_Pairs@data$TrtBin)
-View(trttable)
-
 ## Create panel dataset to create yearly NDVI for each community
+#-------------------------------------------------
+#-------------------------------------------------
 
 varList = c("MaxL_")
-psm_Long <- BuildTimeSeries(dta=psm_Pairs,idField="reu_id",varList_pre=varList,1982,2010,colYears=c("demend_y"),interpYears=c("Slope","Road_dist","Riv_Dist","UF","Elevation","terrai_are","Pop_","MeanT_","MeanP_","MaxT_","MaxP_","MinP_","MinT_","TrtBin"))
+psm_Long <- BuildTimeSeries(dta=dta_Shp,idField="reu_id",varList_pre=varList,1982,2010,colYears=c("demend_y"),interpYears=c("TrtBin"))
 psm_Long$Year <- as.numeric(psm_Long$Year)
 
-#build minimal panel dataset
-# varList = c("MaxL_")
-# psm_Long <- BuildTimeSeries(dta=psm_Pairs,idField="reu_id",varList_pre=varList,1982,2010,colYears=c("demend_y"),interpYears=c("Slope","Road_dist","Riv_Dist","UF","Elevation","terrai_are","Pop_","MeanT_","MeanP_","MaxT_","MaxP_","MinP_","MinT_","TrtBin"))
-# psm_Long$Year <- as.numeric(psm_Long$Year)
-# 
 
+
+
+
+#-------------------------------------------------
+#-------------------------------------------------
 ## View Time Series and GGPlot to create NDVI graphs
+#-------------------------------------------------
+#-------------------------------------------------
 
-#Original Figure 5
+#Original Figure 5 (Dem vs. Non-Dem, with means as well as all comms showing)
 ViewTimeSeries(dta=dta_Shp,IDfield="reu_id",TrtField="TrtBin",idPre="MaxL_[0-9][0-9][0-9][0-9]")
 
-#Figure 5 for JEEM 2nd Resubmission
+#Figure 5 for JEEM 2nd Resubmission (mean only showing)
 ggplot(data = psm_Long, aes(x=Year, y=MaxL_, group=reu_id,linetype=factor(TrtBin))) + 
   geom_line(size=0, linetype=2) +
   stat_summary(fun.y=mean,aes(x=Year,y=MaxL_,group=TrtBin),data=psm_Long,geom='line',size=1.5)+
+  stat_summary(fun.data="mean", conf.int=0.95, geom="errorbar", 
+               width=0.1, linetype=2, colour="blue")+
   theme(axis.text.x=element_text(angle=90,hjust=1))+
   labs(x="Year",y="Max NDVI",linetype="Demarcation")
+
+#scratch
+my_ci <- function(x) data.frame(
+  y=mean(x), 
+  ymin=mean(x) - 1.96 * sd(x), 
+  ymax=mean(x) + 1.96 * sd(x)
+)
+my_mean<-function(x) data.frame(
+  y=mean(x)
+)
+
+ggplot(psm_Long, aes(Year, MaxL_)) + 
+  #geom_point(color="red") +
+  stat_summary(fun.data="my_ci", geom="smooth")
+
+ggplot(psm_Long, aes(x=Year, y=MaxL_,linetype=factor(TrtBin))) +
+  stat_summary(data=dem,fun.data="my_mean",geom="line")+
+  stat_summary(data=nondem,fun.data="my_ci", geom="smooth")
+nondem<-psm_Long[psm_Long$TrtBin==0,]
+dem<-psm_Long[psm_Long$TrtBin==1,]
+
+ggplot(data = psm_Long, aes(x=Year, y=MaxL_, group=reu_id,linetype=factor(TrtBin))) + 
+  geom_line(size=0, linetype=2) +
+  stat_summary(fun.y=mean,aes(x=Year,y=MaxL_,group=TrtBin),data=psm_Long,geom='line',size=1.5)+
+  stat_summary(fun.data="my_ci", geom="smooth")+
+  theme(axis.text.x=element_text(angle=90,hjust=1))+
+  labs(x="Year",y="Max NDVI",linetype="Demarcation")
+
 
 
 # time series graph with year effects stripped out
@@ -151,13 +114,7 @@ plot(psm_Long$resid)
 psm_Long2 <- psm_Long
 psm_Long2 <- psm_Long[c("Year","resid","reu_id","TrtBin")]
 
-#Original Figure 6
-ggplot(data = psm_Long2, aes(x=Year, y=resid, group=reu_id,colour=factor(TrtBin))) + 
-  geom_line(size=.5, linetype=2) +
-  stat_summary(fun.y=mean,aes(x=Year, y=resid, group=TrtBin,colour=factor(TrtBin)),data=psm_Long2,geom='line',size=1.5)+
-  theme(axis.text.x=element_text(angle=90,hjust=1))
-
-#Figure 6 for JEEM 2nd resubmission
+#Figure 6 for JEEM 2nd resubmission (mean only showing)
 ggplot(data = psm_Long2, aes(x=Year, y=resid,linetype=factor(TrtBin))) + 
   geom_line(size=0, linetype=2) +
   stat_summary(fun.y=mean,aes(x=Year, y=resid,group=TrtBin),data=psm_Long2,geom='line',size=1.5) +
@@ -165,5 +122,12 @@ ggplot(data = psm_Long2, aes(x=Year, y=resid,linetype=factor(TrtBin))) +
   labs(x="Year",y="Residual",linetype="Demarcation")
 
 
+## Scratch
+
+#Original Figure 6 (Dem vs. non dem, with year effects stripped out, means + all comm values showing)
+ggplot(data = psm_Long2, aes(x=Year, y=resid, group=reu_id,colour=factor(TrtBin))) + 
+  geom_line(size=.5, linetype=2) +
+  stat_summary(fun.y=mean,aes(x=Year, y=resid, group=TrtBin,colour=factor(TrtBin)),data=psm_Long2,geom='line',size=1.5)+
+  theme(axis.text.x=element_text(angle=90,hjust=1))
 
 
